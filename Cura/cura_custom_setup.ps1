bd8766c15b3dc603be472058926d96d3f5f80f76
Demetrius Apostolopoulos <75643490+dapostol73@users.noreply.github.com>
2022-11-02 09:09:09 -0700

    update for 5.2.1

--- Cura/cura_custom_setup.ps1
+++ Cura/cura_custom_setup.ps1
@@ -1,6 +1,6 @@
 $customLocation = Get-Location
 $customLocation = "$customLocation\*"
-$curaLocation = 'C:\Program Files\Ultimaker Cura 5.0.0\share\cura'
+$curaLocation = 'C:\Program Files\Ultimaker Cura 5.2.1\share\cura'
 #$curaLocation = 'D:\temp\cura'
 $exclude = @('*.ps1')
 $output = Copy-Item -Path $customLocation -Destination $curaLocation -Exclude $exclude -Force -Recurse -PassThru
