e3eae23265c17d30c64dd8915ab659f05dde85d4
Demetrius Apostolopoulos <dapostol73@hotmail.com>
2025-06-04 18:13:19 -0700

    Added support for ASA

--- Cura/cura_custom_setup.ps1
+++ Cura/cura_custom_setup.ps1
@@ -1,7 +1,7 @@
 $scriptLocation = "$PSScriptRoot\*"
 $curaLocation = 'C:\Program Files\Ultimaker Cura 5.10.0\share\cura'
 #$curaLocation = 'D:\temp\cura'
-$exclude = @('*.ps1')
+$exclude = @('*.ps1', '*.py')
 $output = Copy-Item -Path $scriptLocation -Destination $curaLocation -Exclude $exclude -Force -Recurse -PassThru
 
 if ($output)
